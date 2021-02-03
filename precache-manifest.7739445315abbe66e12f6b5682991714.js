c07665687f7ed6ede517bb7b4e37100660da3204
ayaxd <ayaxd0324@gmail.com>
2021-02-03 23:21:57 +1100

    Updates

---
+++ precache-manifest.7739445315abbe66e12f6b5682991714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea1696f0c68be7195bce034290009af",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "566f2cb0ce5f85945fae",
+    "url": "/weather-react/static/css/main.263745c0.chunk.css"
+  },
+  {
+    "revision": "27e342ea511d5955f5c8",
+    "url": "/weather-react/static/js/2.2d85d013.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-react/static/js/2.2d85d013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566f2cb0ce5f85945fae",
+    "url": "/weather-react/static/js/main.dd58d44c.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  }
+]);
