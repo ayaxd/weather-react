59da4bebca72cce18aee7e313b7e447b9cf57166
ayaxd <ayaxd0324@gmail.com>
2021-02-04 00:05:30 +1100

    appid

--- src/utils/OpenWeatherMap/OpenWeatherMap.js
+++ src/utils/OpenWeatherMap/OpenWeatherMap.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const baseURL = 'http://api.openweathermap.org/data/2.5';
 
-// const appid = process.env.APP_ID;
+// const appid = process.env.REACT_APP_API_KEY;
 
 const getWeather = (url, params) =>
   axios.get(`${baseURL}${url}`, {
     params: {
       ...params,
-      appid: process.env.REACT_APP_API_KEY,
+      appid: '4d34fc9919f5210dab7fb0d1f9aa0f9a',
       units: 'metric',
     },
   });
